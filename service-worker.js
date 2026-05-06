328e91f34bddab3dafdc603c9abd90b980ee5733
heikoduemig <43315579+heikoduemig@users.noreply.github.com>
2026-05-06 15:35:59 +0200

    V19

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,11 @@
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>self.clients.claim());
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.claim())
+  );
+});
