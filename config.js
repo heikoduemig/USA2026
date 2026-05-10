b0128f2fd8919615ef6c4126a1680f8f57e6f9c1
heikoduemig <43315579+heikoduemig@users.noreply.github.com>
2026-05-10 10:10:59 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Lokale Konfiguration für Route 66 Music Trip v31.
-// Trage hier deinen referrer-beschränkten Google-Maps-Key ein.
-window.ROUTE66_CONFIG = window.ROUTE66_CONFIG || {
-  GOOGLE_MAPS_API_KEY: ""
-};
