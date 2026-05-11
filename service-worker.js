82b643d673ae3083ccb01e02bc6c24c765f3af4f
heikoduemig <43315579+heikoduemig@users.noreply.github.com>
2026-05-11 09:26:11 +0200

    route66-v35-optimized

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'route66-trip-v35';
+const CACHE_NAME = 'route66-trip-v35-optimized';
 const APP_SHELL = [
   './', './index.html', './manifest.webmanifest', './routeData.v35.js', './musicBars.js'
 ];
